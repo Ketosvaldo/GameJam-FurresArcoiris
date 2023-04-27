dabf07887832385892845f54e95c0d3c2924612b
Eduardo Rejon <eduardo.rejon@uniat.edu.mx>
2023-04-26 23:59:31 -0600

    MenuTemplate
    Menu buttons working

---
+++ Assets/Eduardo/Scripts/MainScreenManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScreenManager : MonoBehaviour
+{
+    [SerializeField] private GameObject panelMenu;
+    [SerializeField] private GameObject panelCredits;
+
+    void Start()
+    {
+        panelMenu.SetActive(true);
+        panelCredits.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scenes/Level_1");
+    }
+
+    public void ShowCredits()
+    {
+        SwitchMenuPanelState();
+        SwitchCreditsPanelState();
+        Debug.Log("Show credits: " + panelCredits.activeInHierarchy);
+    }
+
+    public void ExitGameApp()
+    {
+        Debug.Log("Exit Game");
+        Application.Quit();
+    }
+
+    private void SwitchMenuPanelState()
+    {
+        if (panelMenu.activeInHierarchy)
+        {
+            panelMenu.SetActive(false);
+        }
+        else
+        {
+            panelMenu.SetActive(true);
+        }
+    }
+
+    private void SwitchCreditsPanelState()
+    {
+        if (panelCredits.activeInHierarchy)
+        {
+            panelCredits.SetActive(false);
+        }
+        else
+        {
+            panelCredits.SetActive(true);
+        }
+    }
+}
