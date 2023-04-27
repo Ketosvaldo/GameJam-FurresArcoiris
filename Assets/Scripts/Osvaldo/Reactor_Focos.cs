0e022096a4d7be27ce99bbfd48fd6658f8dda468
Ketosvaldo <ketchulo.conk2013@gmail.com>
2023-04-27 11:31:01 -0600

    Activar/Desactivar Reactor

--- Assets/Scripts/Osvaldo/Reactor_Focos.cs
+++ Assets/Scripts/Osvaldo/Reactor_Focos.cs
@@ -5,11 +5,14 @@ public class Reactor_Focos : MonoBehaviour
 {
     public Image[] image;
     public bool[] isTurnedOn;
-
+    public bool exit;
     public void TurnOn(int index)
     {
         image[index].color = Color.green;
         isTurnedOn[index] = true;
+
+        if (isTurnedOn[0] && isTurnedOn[1])
+            gameObject.SetActive(false);
     }
 
     public void TurnOff(int index)
