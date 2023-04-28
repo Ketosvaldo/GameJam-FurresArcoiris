c1f05bc87c470731731970d01fa3f33a64cd39f5
notmarrow <javierrdzmtz2002@gmail.com>
2023-04-27 21:43:38 -0600

    Fade Effect y cambio a SCR_TP para fade

--- Assets/Alexander/Scripts/Teleport/SCR_Teleport.cs
+++ Assets/Alexander/Scripts/Teleport/SCR_Teleport.cs
@@ -7,12 +7,41 @@ public class SCR_Teleport : MonoBehaviour
     public float posX; //cordenadas en X
     public float posY; //cordenadas en Y
 
+    public GameObject transitionObject;
+    public Collider2D playerStore = null;
+
+    public float transitionTimer = 1, transitionTimerStore;
+    private bool canTeleport = false;
+
+    private void Start() {
+        transitionTimerStore = transitionTimer;
+    }
+
+    private void Update(){
+        if(canTeleport && transitionTimer <= 0){
+            transitionTimer = transitionTimerStore;
+            teleport();
+        }else if(canTeleport){
+            transitionTimer -= Time.deltaTime;
+        }else{
+            transitionTimer = transitionTimerStore;
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "Player" && Input.GetKey(KeyCode.E))
         {
-            collision.transform.position = new Vector2(posX, posY);
-            Debug.Log("kaycode");
+            Movement.canMove = false;
+            playerStore = collision;
+            canTeleport = true;
+            transitionObject.SetActive(true);
         }
+    }
+
+    private void teleport(){
+        playerStore.transform.position = new Vector2(posX, posY);
+        Movement.canMove = true;
+        canTeleport = false;
     }
 }
